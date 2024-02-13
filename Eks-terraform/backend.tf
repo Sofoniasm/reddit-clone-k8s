29476c24db5563dc1b28f7dc201bc3362670550e
Sofonias Mengistu <69535143+Sofoniasm@users.noreply.github.com>
2024-02-13 10:01:42 +0300

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ajay-mrcloudbook777" # Replace with your actual S3 bucket name
+    bucket = "cf-templates-rk5audbax7o5-us-east-1" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
