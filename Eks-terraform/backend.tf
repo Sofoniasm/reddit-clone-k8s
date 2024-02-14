99abfb5d0e32871e8d567ee8eb04eec2383c7c78
Sofonias Mengistu <69535143+Sofoniasm@users.noreply.github.com>
2024-02-14 23:27:54 +0300

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "cf-templates-rk5audbax7o5-us-east-1" # Replace with your actual S3 bucket name
+    bucket = "visualstudiocreatedbucket1234" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
